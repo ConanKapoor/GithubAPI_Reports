fd696889c60ec4d6bef5b7b0200b6ce89ff3f37b
ConanKapoor <kapoor.shivam88@gmail.com>
2017-05-18 00:03:21 +0530

    Added Search bar to filter data. Valar Morghulis/

--- UserData/admin.py
+++ UserData/admin.py
@@ -11,6 +11,7 @@ from .models import user_data
 #Creating Admin Table Layout.
 class User_dataAdmin(admin.ModelAdmin):
     list_display = ['Thumbnail', 'Idx','User_name', 'Full_name', 'Location', 'Blog', 'Public_repos', 'Public_gists', 'Email', 'Followers', 'Following', 'Updated_on']
+    search_fields = ['Idx','User_name', 'Full_name', 'Location', 'Blog', 'Public_repos', 'Public_gists', 'Email', 'Followers', 'Following', 'Updated_on']
 
     class Meta:
         model = user_data
