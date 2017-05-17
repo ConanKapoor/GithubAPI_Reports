7d659b976ce9af4199ef8212be09dfc2e4982efc
ConanKapoor <kapoor.shivam88@gmail.com>
2017-05-17 22:11:29 +0530

    Admin Table up and running Matey

--- UserData/admin.py
+++ UserData/admin.py
@@ -1,6 +1,19 @@
-# -*- coding: utf-8 -*-
+#Author - Shivam Kapoor
+
+#Importing Essentials
 from __future__ import unicode_literals
-
 from django.contrib import admin
+import datetime
+from datetime import date
+from .models import user_data
 
-# Register your models here.
+
+#Creating Admin Table Layout.
+class User_dataAdmin(admin.ModelAdmin):
+    list_display = ['Idx','User_name', 'Full_name', 'Location', 'Blog', 'Public_repos', 'Public_gists', 'Email', 'Followers', 'Following', 'Updated_on']
+
+    class Meta:
+        model = user_data
+
+#Registering Tables for admin page.
+admin.site.register(user_data, User_dataAdmin)
