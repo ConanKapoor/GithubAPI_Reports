0682011878207f4515e29b3f545f13873f7c2832
ConanKapoor <kapoor.shivam88@gmail.com>
2017-05-17 21:00:56 +0530

    Created API to search Github Users.Finally :]

--- UserData/models.py
+++ UserData/models.py
@@ -1,6 +1,25 @@
+#Author - Shivam Kapoor
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.db import models
 
-# Create your models here.
+#Creating user_data model to store data.
+class user_data(models.Model):
+    Idx = models.PositiveIntegerField()
+    User_name = models.CharField(max_length=200)
+    Full_name = models.CharField(max_length=250, blank=True, null=True)
+    Location = models.CharField(max_length=200)
+    Blog = models.CharField(max_length=200)
+    Public_repos = models.PositiveIntegerField()
+    Public_gists = models.PositiveIntegerField()
+    Email = models.CharField(max_length=250, blank=True, null=True)
+    Followers = models.PositiveIntegerField()
+    Following = models.PositiveIntegerField()
+    Updated_on = models.DateField()
+
+    class Meta(object):
+        verbose_name = '1_User Data'
+        verbose_name_plural = '1_User Data'
+
+    def __unicode__(self): #Python2 declaration
+        return (str(self.Full_name + str(self.Idx)))
--- UserData/views.py
+++ UserData/views.py
@@ -1,6 +1,20 @@
-# -*- coding: utf-8 -*-
+#Author - Shivam Kapoor
+
+#Importing Essentials
 from __future__ import unicode_literals
-
 from django.shortcuts import render
+import requests, json, dateutil.parser
+from .models import *
 
-# Create your views here.
+#View to retrieve user data from GithubAPI.
+def userdata_retrieval(request):
+    content = {}
+    if request.method == 'POST':
+        username = request.POST.get('user')
+
+        #Giving request to GithubAPI and converting to JSON format.
+        req = requests.get('https://api.github.com/users/'+username)
+        content = req.json()
+        content['created_at'] = dateutil.parser.parse(content['created_at'])
+
+    return render(request, 'UserData/userdata.html', content)
